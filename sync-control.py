e67856d31f38468ebb36748ddd25e78a9607e48f
Trever Fischer <wm161@wm161.net>
2008-08-04 18:31:38 -0400

    Now queries the server for a list of files

--- sync-control.py
+++ sync-control.py
@@ -2,27 +2,28 @@
 from PyQt4 import QtCore, QtGui
 from Ui_MainWindow import Ui_MainWindow
 import os, sys
-from GitInterface import GitInterface
+from dbus.mainloop.qt import DBusQtMainLoop
+import dbus
 
 class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
-	def __init__(self):
-		home = os.environ['HOME']
-		self.git = GitInterface(home+"/.home-sync/git",home,home+"/.home-sync/ignore")
-		
-		QtGui.QMainWindow.__init__(self)
-		self.setupUi(self)
-		self.connect(self.actionBackup_Now,QtCore.SIGNAL('triggered()'), self.showSettings)
-		return
-		self.statusList = QtGui.QStandardItemModel(self)
-		for file in self.git.files():
-			self.statusList.append(file)
-		self.revisionView.setModel(self.statusList)
-	
-	def showSettings(self):
-		print "Yep!"
+  def __init__(self):
+    QtGui.QMainWindow.__init__(self)
+    self.setupUi(self)
+    self.connect(self.actionBackup_Now,QtCore.SIGNAL('triggered()'), self.showSettings)
+    bus = dbus.SessionBus()
+    serverObj = bus.get_object('net.wm161.HomeSync', '/Server');
+    self.server = dbus.Interface(serverObj, dbus_interface='net.wm161.HomeSync.Server')
+    self.statusList = QtGui.QStringListModel(self)
+    self.statusList.setStringList(self.server.ListFiles(2))
+    #for file in self.server.ListFiles(2):
+    #  self.statusList.append(file)
+    self.fileView.setModel(self.statusList)
+
+  def showSettings(self):
+    print "Yep!"
 
 if __name__ == "__main__":
-	app = QtGui.QApplication(sys.argv)
-	mw = MainWindow()
-	mw.show()
-	sys.exit(app.exec_())
+  app = QtGui.QApplication(sys.argv)
+  mw = MainWindow()
+  mw.show()
+  sys.exit(app.exec_())
